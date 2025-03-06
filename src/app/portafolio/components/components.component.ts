041cd03521d4e0c9ad1b694fb79f0c92a79dbe07
Cristian <389336@alu.murciaeduca.es>
2025-03-06 12:04:48 +0100

    Añado el boton para el tema claro y oscuro con su funcionalidad, problemas con el cambio de tema a oscuro pendiente de solucionar

--- src/app/portafolio/components/components.component.ts
+++ src/app/portafolio/components/components.component.ts
@@ -1,11 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-components',
-  imports: [],
   templateUrl: './components.component.html',
-  styleUrl: './components.component.css'
+  styleUrls: ['./components.component.css'],
 })
-export class ComponentsComponent {
+export class ComponentsComponent implements OnInit {
+  isMenuVisible: boolean = false;
 
+  ngOnInit(): void {
+    document.addEventListener('click', this.handleOutsideClick.bind(this));
+  }
+
+  toggleMenu(): void {
+    this.isMenuVisible = !this.isMenuVisible;
+  }
+
+  handleOutsideClick(event: Event): void {
+    const themeToggleBtn = document.getElementById('theme-toggle-btn');
+    const themesMenu = document.getElementById('themes-menu');
+    if (
+      themeToggleBtn &&
+      themesMenu &&
+      !themeToggleBtn.contains(event.target as Node) &&
+      !themesMenu.contains(event.target as Node)
+    ) {
+      this.isMenuVisible = false;
+    }
+  }
+
+  setTheme(theme: string): void {
+    const htmlElement = document.documentElement;
+    if (theme === 'Claro') {
+      htmlElement.classList.remove('dark');
+    } else if (theme === 'Oscuro') {
+      htmlElement.classList.add('dark');
+    }
+    this.isMenuVisible = false;
+  }
 }
