9e143c8a2c890f37554e4b6ad4cd2c299bbd24c5
Cristian <389336@alu.murciaeduca.es>
2025-04-02 12:02:23 +0200

    Introduzco el marco de proyectos backend con su boton

--- src/app/portafolio/pages/proyectos-page/proyectos-page.component.ts
+++ src/app/portafolio/pages/proyectos-page/proyectos-page.component.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterModule, RouterOutlet } from '@angular/router';
-
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-proyectos-page',
@@ -10,5 +10,27 @@ import { RouterModule, RouterOutlet } from '@angular/router';
   styleUrl: './proyectos-page.component.css',
 })
 export class ProyectosPageComponent {
+  isModalOpen: boolean = false;
+  videoUrl: SafeResourceUrl;
+  videoLiga: SafeResourceUrl;
 
+  constructor(private sanitizer: DomSanitizer) {
+    // URL del video de YouTube futbol
+    this.videoUrl = this.sanitizer.bypassSecurityTrustResourceUrl(
+      'https://www.youtube.com/embed/0j2fgNlc-bw?si=Z6zEoBqOcMy1LZpj'
+    );
+    this.videoLiga = this.sanitizer.bypassSecurityTrustResourceUrl(
+      'https://www.youtube.com/embed/IrkJljILrzQ?si=KlKZCPjFiYX4JeMU'
+    );
+  }
+
+  // Abrir el modal
+  openModal(): void {
+    this.isModalOpen = true;
+  }
+
+  // Cerrar el modal
+  closeModal(): void {
+    this.isModalOpen = false;
+  }
 }
